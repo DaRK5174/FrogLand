c2e35e0b8926dccd08e7219bb95d46483fdfb50d
keksyk <true.gameof@gmail.com>
2024-12-08 22:07:03 +0500

    Enemy
    аркаша тупой гей заставил меня делать проект:(

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float speed;
+    public Rigidbody2D rb;
+
+    public Transform player;
+    public Transform FeetPos;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+    }
+    
+}
